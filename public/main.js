b9d0e260d778649d34ddf850dafabb1f627e0df1
rwmurray10 <145693772+rwmurray10@users.noreply.github.com>
2024-01-18 01:56:41 +0000

    Updated with final adjustments

--- public/main.js
+++ public/main.js
@@ -1,2 +1,33 @@
-// Write JavaScript here.
-console.log('main.js loaded successfully!')
+console.log("Hello world!");
+function myFunction() {
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
+}
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  console.log("Hello world!");
+
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
